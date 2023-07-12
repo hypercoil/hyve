2f47fad3a721ad4618782b11c126208070d93c7c
Rastko Ciric <rastko@stanford.edu>
2023-07-12 15:43:50 -0700

    (MAINT) boilerplate noxfile

---
+++ noxfile.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Noxfile
+"""
+import nox
+
+@nox.session()
+def clean(session):
+    session.install('coverage[toml]')
+    session.run('coverage', 'erase')
+
+@nox.session(python=["3.10", "3.11"])
+def tests(session):
+    session.install('.')
+    session.install('coverage[toml]')
+    session.install('pytest')
+    session.install('pytest-cov')
+    session.install('ruff')
+    session.run(
+        'pytest',
+        '--cov', 'hyve',
+        '--cov-append',
+        'tests/',
+    )
+    session.run('ruff', 'check', 'src/hyve')
+
+@nox.session()
+def report(session):
+    session.install('coverage[toml]')
+    session.run(
+        'coverage',
+        'report', '--fail-under=80',
+        "--omit='*test*,*__init__*'",
+    )
+    session.run(
+        'coverage',
+        'html',
+        "--omit='*test*,*__init__*'",
+    )
+    session.run(
+        'coverage',
+        'xml',
+        "--omit='*test*,*__init__*'",
+    )
