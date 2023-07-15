6ecdf07101aa78af8bc3a3b740249587b5da6c0f
Rastko Ciric <rastko@stanford.edu>
2023-07-14 17:07:47 -0700

    (TEST) HTML renderer with `trame`

--- tests/test_surfplot.py
+++ tests/test_surfplot.py
@@ -78,7 +78,7 @@ def test_parcellation():
         plot_to_image(),
         save_screenshots(
             fname_spec=(
-                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_mode-face'
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_cmap-network_mode-face'
             ),
         ),
     )
@@ -106,7 +106,7 @@ def test_parcellation():
         plot_to_image(),
         save_screenshots(
             fname_spec=(
-                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_mode-vertex'
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_cmap-network_mode-vertex'
             ),
         ),
     )
@@ -129,143 +129,160 @@ def test_parcellation():
         output_dir='/tmp',
     )
 
-# @pytest.mark.ci_unsupported
-# def test_parcellation_modal_cmap():
-#     i_chain = ichain(
-#         surf_from_archive(),
-#         scalars_from_gifti('parcellation', plot=True),
-#         parcellate_colormap('modal', 'parcellation')
-#     )
-#     o_chain = ochain(
-#         split_chain(
-#             omap(
-#                 plot_to_image(),
-#                 mapping={
-#                     'basename': ('/tmp/leftmodal', '/tmp/rightmodal'),
-#                     'hemisphere': ('left', 'right'),
-#                 }
-#             ),
-#             omap(
-#                 plot_to_image(),
-#                 mapping={
-#                     'basename': ('/tmp/leftmodal', '/tmp/rightmodal'),
-#                     'hemisphere': ('left', 'right'),
-#                     'views': (((-20, 0, 0),), (((65, 65, 0), (0, 0, 0), (0, 0, 1)),))
-#                 }
-#             ),
-#         )
-#     )
-#     f = iochain(automap_unified_plotter_p, i_chain, o_chain)
-#     f(
-#         template="fsLR",
-#         load_mask=True,
-#         parcellation_gifti_left=pkgrf(
-#             'hyve',
-#             'data/examples/nullexample_L.gii'
-#         ),
-#         parcellation_gifti_right=pkgrf(
-#             'hyve',
-#             'data/examples/nullexample_R.gii'
-#         ),
-#         surf_projection=('veryinflated',),
-#         surf_scalars_boundary_color='black',
-#         surf_scalars_boundary_width=5,
-#         hemisphere=['left', 'right'],
-#     )
+@pytest.mark.ci_unsupported
+def test_parcellation_modal_cmap():
+    chain = ichain(
+        surf_from_archive(),
+        scalars_from_gifti('parcellation', plot=True),
+        parcellate_colormap('modal', 'parcellation'),
+        vertex_to_face('parcellation'),
+        plot_to_image(),
+        save_screenshots(
+            fname_spec=(
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_cmap-modal_mode-face'
+            ),
+        ),
+    )
+    plot_f = iochain(automap_unified_plotter_p, chain)
+    plot_f(
+        template="fsLR",
+        load_mask=True,
+        parcellation_gifti_left=pkgrf(
+            'hyve',
+            'data/examples/nullexample_L.gii'
+        ),
+        parcellation_gifti_right=pkgrf(
+            'hyve',
+            'data/examples/nullexample_R.gii'
+        ),
+        surf_projection=('veryinflated',),
+        hemisphere=['left', 'right'],
+        views=[
+            'lateral', 'medial', 'ventral', 'dorsal', 'anterior', 'posterior',
+            (-20, 0, 0), ((65, 65, 0), (0, 0, 0), (0, 0, 1))
+        ],
+        output_dir='/tmp',
+    )
 
-# @pytest.mark.ci_unsupported
-# def test_parcellation_html():
-#     i_chain = ichain(
-#         surf_from_archive(),
-#         scalars_from_cifti('parcellation', plot=True),
-#         parcellate_colormap('network', 'parcellation')
-#     )
-#     o_chain = ochain(
-#         omap(
-#             plot_to_html(backend="panel"),
-#             mapping={
-#                 "filename": ('/tmp/left.html', '/tmp/right.html'),
-#             }
-#         ),
-#     )
-#     f = iochain(automap_unified_plotter_p, i_chain, o_chain)
-#     f(
-#         template="fsLR",
-#         load_mask=True,
-#         parcellation_cifti=pkgrf(
-#             'hyve',
-#             'data/examples/nullexample.nii'
-#         ),
-#         surf_projection=['veryinflated'],
-#         surf_scalars_boundary_color='black',
-#         surf_scalars_boundary_width=5,
-#         hemisphere=['left', 'right'],
-#     )
+    chain = ichain(
+        surf_from_archive(),
+        scalars_from_cifti('parcellation', plot=True),
+        parcellate_colormap('modal', 'parcellation'),
+        plot_to_image(),
+        save_screenshots(
+            fname_spec=(
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_cmap-modal_mode-vertex'
+            ),
+        ),
+    )
+    plot_f = iochain(automap_unified_plotter_p, chain)
+    plot_f(
+        template="fsLR",
+        load_mask=True,
+        parcellation_cifti=pkgrf(
+            'hyve',
+            'data/examples/nullexample.nii'
+        ),
+        surf_projection=('veryinflated',),
+        surf_scalars_boundary_color='black',
+        surf_scalars_boundary_width=5,
+        hemisphere=['left', 'right'],
+        views=[
+            'lateral', 'medial', 'ventral', 'dorsal', 'anterior', 'posterior',
+            (-20, 0, 0), ((65, 65, 0), (0, 0, 0), (0, 0, 1))
+        ],
+        output_dir='/tmp',
+    )
 
-# @pytest.mark.ci_unsupported
-# def test_parcellated_scalars():
-#     i_chain = ichain(
-#         surf_from_archive(),
-#         resample_to_surface('gmdensity', template='fsLR'),
-#         scalars_from_cifti('parcellation'),
-#         parcellate_scalars('gmdensity', 'parcellation'),
-#     )
-#     o_chain = ochain(
-#         omap(
-#             plot_to_image(),
-#             mapping={
-#                 "basename": ('/tmp/left_density_parc', '/tmp/right_density_parc'),
-#                 "hemisphere": ('left', 'right'),
-#             }
-#         )
-#     )
-#     f = iochain(automap_unified_plotter_p, i_chain, o_chain)
-#     out = f(
-#         template="fsLR",
-#         load_mask=True,
-#         parcellation_cifti=pkgrf(
-#             'hypercoil',
-#             'viz/resources/nullexample.nii'
-#         ),
-#         gmdensity_nifti=tflow.get(
-#             template='MNI152NLin2009cAsym',
-#             suffix='probseg',
-#             label="GM",
-#             resolution=2
-#         ),
-#         surf_projection=['inflated'],
-#         surf_scalars_clim=(0.2, 0.9),
-#         hemisphere=['left', 'right'],
-#     )
-#     assert len(out.keys()) == 1
-#     assert "screenshots" in out.keys()
+@pytest.mark.ci_unsupported
+def test_parcellation_html():
+    chain = ichain(
+        surf_from_archive(),
+        scalars_from_cifti('parcellation', plot=True),
+        parcellate_colormap('network', 'parcellation'),
+        vertex_to_face('parcellation'),
+        plot_to_html(
+            fname_spec=(
+                'scalars-{scalars}_hemisphere-{hemisphere}_cmap-network'
+            ),
+        ),
+    )
+    plot_f = iochain(automap_unified_plotter_p, chain)
+    plot_f(
+        template="fsLR",
+        load_mask=True,
+        parcellation_cifti=pkgrf(
+            'hyve',
+            'data/examples/nullexample.nii'
+        ),
+        surf_projection=['veryinflated'],
+        # surf_scalars_boundary_color='black',
+        # surf_scalars_boundary_width=5,
+        hemisphere=['left', 'right'],
+        window_size=(800, 800),
+        output_dir='/tmp',
+    )
 
-#     parcellated = np.random.rand(400)
-#     i_chain = ichain(
-#         surf_from_archive(),
-#         scalars_from_cifti('parcellation'),
-#         scatter_into_parcels('scalars', 'parcellation'),
-#     )
-#     o_chain = ochain(
-#         omap(
-#             plot_to_image(),
-#             mapping={
-#                 "basename": ('/tmp/left_noise_parc', '/tmp/right_noise_parc'),
-#                 "hemisphere": ('left', 'right'),
-#             }
-#         )
-#     )
-#     f = iochain(automap_unified_plotter_p, i_chain, o_chain)
-#     out = f(
-#         template="fsLR",
-#         load_mask=True,
-#         parcellation_cifti=pkgrf(
-#             'hypercoil',
-#             'viz/resources/nullexample.nii'
-#         ),
-#         parcellated=parcellated,
-#         surf_projection=['inflated'],
-#         surf_scalars_clim=(0, 1),
-#         cmap='inferno',
-#         hemisphere=['left', 'right'],
-#     )
+@pytest.mark.ci_unsupported
+def test_parcellated_scalars():
+    chain = ichain(
+        surf_from_archive(),
+        resample_to_surface('gmdensity', template='fsLR'),
+        scalars_from_cifti('parcellation'),
+        parcellate_scalars('gmdensity', 'parcellation'),
+        vertex_to_face('gmdensityParcellated', interpolation='mode'),
+        plot_to_image(),
+        save_screenshots(
+            fname_spec=(
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_parcellation-null'
+            ),
+        ),
+    )
+    plot_f = iochain(automap_unified_plotter_p, chain)
+    plot_f(
+        template="fsLR",
+        load_mask=True,
+        parcellation_cifti=pkgrf(
+            'hyve',
+            'data/examples/nullexample.nii',
+        ),
+        gmdensity_nifti=tflow.get(
+            template='MNI152NLin2009cAsym',
+            suffix='probseg',
+            label="GM",
+            resolution=2
+        ),
+        surf_projection=['inflated'],
+        surf_scalars_clim=(0.2, 0.9),
+        hemisphere=['left', 'right'],
+        output_dir='/tmp',
+    )
+
+    parcellated = np.random.rand(400)
+    chain = ichain(
+        surf_from_archive(),
+        scalars_from_cifti('parcellation'),
+        scatter_into_parcels('noise', 'parcellation'),
+        vertex_to_face('noise', interpolation='mode'),
+        plot_to_image(),
+        save_screenshots(
+            fname_spec=(
+                'scalars-{scalars}_hemisphere-{hemisphere}_view-{view}_parcellation-null'
+            ),
+        ),
+    )
+    plot_f = iochain(automap_unified_plotter_p, chain)
+    plot_f(
+        template="fsLR",
+        load_mask=True,
+        parcellation_cifti=pkgrf(
+            'hyve',
+            'data/examples/nullexample.nii',
+        ),
+        noise_parcellated=parcellated,
+        surf_projection=['inflated'],
+        surf_scalars_clim=(0, 1),
+        surf_scalars_cmap='inferno',
+        hemisphere=['left', 'right'],
+        output_dir='/tmp',
+    )
